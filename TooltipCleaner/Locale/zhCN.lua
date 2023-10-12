031035deb3a81627f9e535f8657c5c4b7ed5a4e4
EK <eke00372@gmail.com>
2023-10-12 09:10:24 +0800

    Update zhCN.lua

--- TooltipCleaner/Locale/zhCN.lua
+++ TooltipCleaner/Locale/zhCN.lua
@@ -1,4 +1,18 @@
 local Addon, ns = ...
 
 local L = LibStub("AceLocale-3.0"):NewLocale(Addon, "zhCN")
-if (not L) then return end
+
+L["Hide Blizzard vendor prices."] = "隐藏物品售价"
+L["Hides vendors prices from default tooltip when enabled."] = "隐藏暴雪提供的物品售价"
+L["Hide superflous item requirements."] = "隐藏物品条件"
+L["Hides item requirements and equip criteria you have matched."] = "当你满足物品的使用条件或装备条件时，隐藏条件的说明文本"
+L["Hide full durability."] = "隐藏完整的耐久度"
+L["Hides item durability when the item has full durability."] = "当物品完好无损，隐藏耐久度。"
+L["Hide grayed out item stats."] = "隐藏灰色属性"
+L["Hides grayed out item stats your character can't currently use."] = "隐藏角色目前的专精无法使用的灰色属性"
+L["Hide unachieved set bonuses."] = "隐藏未获得的套装效果"
+L["Hides set bonuses you haven't collected enough items to receive yet."] = "隐藏未获得的套装效果"
+L["Hide factions."] = "隐藏阵营"
+L["Hides a unit's faction allegiance."] = "隐藏阵营"
+L["Hide PvP status."] = "隐藏 PvP 状态"
+L["Hides a unit's PvP status."] "隐藏 PvP 状态"
